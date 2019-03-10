0739850436a691b63a3e56a1739068f4bf5b7812
sunnythree <1318288270@qq.com>
2019-03-10 11:49:36 +0800

    添加必须实现的方法

--- src/cupcnn/layer/RecurrentLayer.java
+++ src/cupcnn/layer/RecurrentLayer.java
@@ -1,5 +1,64 @@
 package cupcnn.layer;
 
-public class RecurrentLayer {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import cupcnn.Network;
+import cupcnn.data.Blob;
+
+public class RecurrentLayer extends Layer {
+
+	public RecurrentLayer(Network network) {
+		super(network);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Blob createOutBlob() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Blob createDiffBlob() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void backward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveModel(ObjectOutputStream out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadModel(ObjectInputStream in) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/cupcnn/layer/RnnCell.java
+++ src/cupcnn/layer/RnnCell.java
@@ -1,5 +1,64 @@
 package cupcnn.layer;
 
-public class RnnCell {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import cupcnn.Network;
+import cupcnn.data.Blob;
+
+public class RnnCell extends Layer{
+
+	public RnnCell(Network network) {
+		super(network);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Blob createOutBlob() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Blob createDiffBlob() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void backward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveModel(ObjectOutputStream out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadModel(ObjectInputStream in) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
